775f68a43808abb8310c1ea9fdeb5737f6e7744f
Murilo Henrique <murilo.troxa@hotmail.com>
2022-03-28 18:49:42 -0300

    28/03/2022 - TODAS ATIVIDADES

---
+++ src/com/company/comecando/atividades/Atividade18.java
@@ -0,0 +1,20 @@
+package com.company.comecando.atividades;
+
+import java.util.Scanner;
+
+public class Atividade18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o tamanho do arquivo:   ");
+        Double tamanhoDoArquivoEmMB = scanner.nextDouble();
+        System.out.print("Digite a velocidade da sua internet: ");
+        Double velocidadeDaInternetEmMBPS = scanner.nextDouble();
+        Byte ValorParaTransformar = 8;
+        Double velocidadeDeDownloadEmMB = velocidadeDaInternetEmMBPS / 8;
+        Double tempoBaixadoEmSegundos = tamanhoDoArquivoEmMB / velocidadeDeDownloadEmMB;
+        Byte dividirDeSegundoPraMinuto = 60;
+        Double tempoBaixadoEmMinutos = tempoBaixadoEmSegundos / dividirDeSegundoPraMinuto;
+        System.out.print("O tempo que o arquivo vai ser baixado em segundos é: "+ tempoBaixadoEmSegundos);
+        System.out.print("O tempo que o arquivo vai ser baixado em minutos é: " + tempoBaixadoEmMinutos);
+        scanner.close();
+    }}
