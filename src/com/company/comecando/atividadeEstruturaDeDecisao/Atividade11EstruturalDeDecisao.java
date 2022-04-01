9a140cd4e2e49028a4c8b9e0caa236a3e8a61561
Murilo Henrique <murilo.troxa@hotmail.com>
2022-04-01 20:49:42 -0300

    28/03/2022 - TODAS ATIVIDADES

--- src/com/company/comecando/atividadeEstruturaDeDecisao/Atividade11EstruturalDeDecisao.java
+++ src/com/company/comecando/atividadeEstruturaDeDecisao/Atividade11EstruturalDeDecisao.java
@@ -41,7 +41,7 @@ public class Atividade11EstruturalDeDecisao {
             salarioComAumento = valorSalarioAntesDoAumento + aumentoSalario1500;
             System.out.println("O salário antes do reajuste era: " + valorSalarioAntesDoAumento);
             System.out.println("O percentual de aumento aplicado é: " + aumentoApos1500);
-            System.out.println("O valor do aumento é de: " + aumentoSalarioApos1500);
+            System.out.println("O valor do aumento é de:  " + aumentoSalarioApos1500);
             System.out.println("O salário reajustado é de: "+ salarioComAumento);
         } else {
             System.out.print("Valor indefinido");
