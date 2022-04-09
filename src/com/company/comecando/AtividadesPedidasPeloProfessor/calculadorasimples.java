e7bcf3a990bbf29f184d97317ba611dbe8028af9
Murilo Henrique <murilo.troxa@hotmail.com>
2022-04-08 21:08:09 -0300

    28/03/2022 - TODAS ATIVIDADES

--- src/com/company/comecando/AtividadesPedidasPeloProfessor/calculadorasimples.java
+++ src/com/company/comecando/AtividadesPedidasPeloProfessor/calculadorasimples.java
@@ -9,7 +9,7 @@ public class calculadorasimples {
         double numero1 = scanner.nextDouble();
         System.out.print("digite outro numero: ");
         double numero2 = scanner.nextDouble();
-        System.out.print("Digite o sinal desejado. 1 para soma, 2 para subtração, 3 para divisão, 4 para multiplicação.");
+        System.out.print("Digite o sinal desejado. 1 para soma, 2 para subtração, 3 para divisão, 4 para multiplicação: ");
         byte sinalDesejado = scanner.nextByte();
         double resultado;
         if (sinalDesejado == 1) {
@@ -19,7 +19,7 @@ public class calculadorasimples {
             resultado = numero1 - numero2;
             System.out.print("ambos numeros subtraidos são: " + resultado);
         } else if (sinalDesejado == 3) {
-            resultado = numero1 / numero2;
+            resultado = numero2 / numero1;
             System.out.print("ambos numeros divididos são: " + resultado);
         } else if (sinalDesejado == 4) {
             resultado = numero1 * numero2;
