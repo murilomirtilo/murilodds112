917d9fe19e825f2cddd23fd901c292336dff4ced
Murilo Henrique <murilo.troxa@hotmail.com>
2022-04-20 20:37:03 -0300

    28/03/2022 - TODAS ATIVIDADES

--- src/com/company/comecando/AtividadesPedidasPeloProfessor/CincoatividadesImportantesDoDia.java
+++ src/com/company/comecando/AtividadesPedidasPeloProfessor/CincoatividadesImportantesDoDia.java
@@ -7,8 +7,7 @@ public class CincoatividadesImportantesDoDia {
     String[] cincoTarefasImportantes = new String[5];
     for(int i = 0; i < cincoTarefasImportantes.length; i++) {
         System.out.print("Tarefa " + i + ":");
-        cincoTarefasImportantes[i] = scanner.next();
-        }
+        cincoTarefasImportantes[i] = scanner.next(); }
     System.out.println(Arrays.toString(cincoTarefasImportantes));
     }
 }
