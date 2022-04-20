464a52d775d0e5a9f516505a19b762f980bd4d50
Murilo Henrique <murilo.troxa@hotmail.com>
2022-04-20 20:36:16 -0300

    28/03/2022 - TODAS ATIVIDADES

--- src/com/company/comecando/AtividadesPedidasPeloProfessor/CincoatividadesImportantesDoDia.java
+++ src/com/company/comecando/AtividadesPedidasPeloProfessor/CincoatividadesImportantesDoDia.java
@@ -5,16 +5,10 @@ public class CincoatividadesImportantesDoDia {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     String[] cincoTarefasImportantes = new String[5];
-    System.out.print("Informe as 1ª de 5 tarefas importantes no seu dia: ");
-    cincoTarefasImportantes [0] = scanner.nextLine();
-    System.out.print("Informe as 2ª de 5 tarefas importantes no seu dia: ");
-    cincoTarefasImportantes [1] = scanner.nextLine();
-    System.out.print("Informe as 3ª de 5 tarefas importantes no seu dia: ");
-    cincoTarefasImportantes [2] = scanner.nextLine();
-    System.out.print("Informe as 4ª de 5 tarefas importantes no seu dia: ");
-    cincoTarefasImportantes [3] = scanner.nextLine();
-    System.out.print("Informe as 5ª de 5 tarefas importantes no seu dia: ");
-    cincoTarefasImportantes [4] = scanner.nextLine();
+    for(int i = 0; i < cincoTarefasImportantes.length; i++) {
+        System.out.print("Tarefa " + i + ":");
+        cincoTarefasImportantes[i] = scanner.next();
+        }
     System.out.println(Arrays.toString(cincoTarefasImportantes));
     }
 }
