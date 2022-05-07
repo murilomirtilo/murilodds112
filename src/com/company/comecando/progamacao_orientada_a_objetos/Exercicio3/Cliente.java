dd7080bd57f36427e2cee1de9b86883b3f32de83
Murilo Henrique <murilo.troxa@hotmail.com>
2022-05-06 21:24:48 -0300

    28/03/2022 - TODAS ATIVIDADES

---
+++ src/com/company/comecando/progamacao_orientada_a_objetos/Exercicio3/Cliente.java
@@ -0,0 +1,41 @@
+package com.company.comecando.progamacao_orientada_a_objetos.Exercicio3;
+
+public class Cliente {
+
+    String codigo;
+    Integer subtotal;
+    Integer desconto;
+    Integer total;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Integer subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Integer getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Integer desconto) {
+        this.desconto = desconto;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+}
---
+++ src/com/company/comecando/progamacao_orientada_a_objetos/Exercicio3/Encapsulamento.java
@@ -0,0 +1,20 @@
+package com.company.comecando.progamacao_orientada_a_objetos.Exercicio3;
+
+import org.omg.CORBA.Object;
+
+import java.util.Objects;
+
+public class Encapsulamento {
+    public static void main(String[] args) {
+    Cliente cliente = new Cliente();
+
+    cliente.setCodigo("Sanduiche");
+    cliente.setSubtotal(100);
+    cliente.setDesconto(10);
+    cliente.setTotal(90);
+
+    public Object mostrarComanda (Cliente cliente) {
+        System.out.println("O código é: " + cliente.codigo);
+        }
+    }
+}
