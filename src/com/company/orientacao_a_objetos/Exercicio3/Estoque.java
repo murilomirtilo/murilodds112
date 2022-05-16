312dcd3bd37beb35accdefa6200cf24498b13991
Murilo Henrique <murilo.troxa@hotmail.com>
2022-05-16 18:50:50 -0300

    28/03/2022 - TODAS ATIVIDADES

---
+++ src/com/company/orientacao_a_objetos/Exercicio3/Estoque.java
@@ -0,0 +1,15 @@
+package com.company.orientacao_a_objetos.Exercicio3;
+
+public class Estoque {
+
+    Produto[] produtos;
+
+    void listarProdutos() {
+        System.out.println("\nProdutos em estoque");
+        System.out.println("------------------------");
+        for (int i = 0; i < produtos.length; i++) {
+            produtos[i].descrever();
+        }
+
+    }
+}
