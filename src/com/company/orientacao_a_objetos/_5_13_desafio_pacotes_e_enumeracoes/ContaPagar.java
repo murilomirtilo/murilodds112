8fe4470dd2413cbe15be87e9bcb9597dcb94d6a6
Murilo Henrique <murilo.troxa@hotmail.com>
2022-05-23 20:03:04 -0300

    28/03/2022 - TODAS ATIVIDADES

--- src/com/company/orientacao_a_objetos/_5_13_desafio_pacotes_e_enumeracoes/ContaPagar.java
+++ src/com/company/orientacao_a_objetos/_5_13_desafio_pacotes_e_enumeracoes/ContaPagar.java
@@ -5,50 +5,43 @@ import com.company.orientacao_a_objetos.Atividades.Exemplo.Desafio.Fornecedor;
 import static com.company.orientacao_a_objetos._5_13_desafio_pacotes_e_enumeracoes.SituacaoConta.*;
 
 public class ContaPagar {
-    String descricao;
+    static String descricao;
     Double valor;
     String dataVencimento;
     Fornecedor fornecedor;
+
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-
     public Double getValor() {
         return valor;
     }
-
     public void setValor(Double valor) {
         this.valor = valor;
     }
-
     public String getDataVencimento() {
         return dataVencimento;
     }
-
     public void setDataVencimento(String dataVencimento) {
         this.dataVencimento = dataVencimento;
     }
-
     public Fornecedor getFornecedor() {
         return fornecedor;
     }
-
     public void setFornecedor(Fornecedor fornecedor) {
         this.fornecedor = fornecedor;
     }
-
     public ContaPagar(String descricao, Double valor, String dataVencimento, Fornecedor fornecedor) {
         this.descricao = descricao;
         this.valor = valor;
         this.dataVencimento = dataVencimento;
         this.fornecedor = fornecedor;
     }
-
     public ContaPagar() {
+        situacaoConta = PENDENTE;
     }
 
 
@@ -61,25 +54,25 @@ public class ContaPagar {
 
     public void cancelar() {
         if (this.situacaoConta == CANCELADA) {
-            System.out.println("CONTA JA CANCELADA, IMPOSSIBILITADO DE CANCELAR");
+            System.out.println("CONTA "+ "'" + descricao + "'" + " JA CANCELADA, IMPOSSIBILITADO DE CANCELAR");
         }
         if (this.situacaoConta == PAGA) {
-            System.out.println("CONTA JA PAGA, IMPOSSIBILITADO DE CANCELAR");
+            System.out.println("CONTA " + "'" + descricao + "'" + " JA PAGA, IMPOSSIBILITADO DE CANCELAR");
         }
         this.situacaoConta = CANCELADA;
-        System.out.println("Conta cancelada");
+        System.out.println("Conta " + "'" + descricao + "'" + " cancelada");
     }
     public void pagar() {
         if (this.situacaoConta == CANCELADA) {
-            System.out.println("Não pode pagar, conta já cancelada");
+            System.out.println("Não pode pagar " + "'" + descricao + "'" + " conta já cancelada");
             return;
         }
         if (this.situacaoConta == PAGA) {
-            System.out.println("Não pode pagar, conta já paga");
+            System.out.println("Não pode pagar " + "'" + descricao + "'" + " conta já paga");
             return;
         }
         SituacaoConta situacaoConta = PENDENTE;
-        System.out.println("Conta Paga");
+        System.out.println("Conta paga " + "'" + descricao + "'");
         this.situacaoConta = PAGA;
 
     }
--- src/com/company/orientacao_a_objetos/_5_13_desafio_pacotes_e_enumeracoes/SituacaoConta.java
+++ src/com/company/orientacao_a_objetos/_5_13_desafio_pacotes_e_enumeracoes/SituacaoConta.java
@@ -1,7 +1,7 @@
 package com.company.orientacao_a_objetos._5_13_desafio_pacotes_e_enumeracoes;
 
 public enum SituacaoConta {
-    PENDENTE(),PAGA(),CANCELADA();
+    PENDENTE(),PAGA(),CANCELADA(), descricao;
 
 
 }
